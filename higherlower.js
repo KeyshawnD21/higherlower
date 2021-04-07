f90ed5b97c08a9f1102af039573dbd290abb499b
KeyshawnD21 <keyshawn77@gmail.com>
2021-04-07 14:34:22 -0500

    Initial commit

---
+++ higherlower.js
@@ -0,0 +1,52 @@
+var maxNumber = window.prompt("What should the maximum guess be?");
+
+while(true) {
+   
+    if(maxNumber < 1) {
+        alert('Invalid Entry');
+
+        var maxNumber = prompt("Choose a positive number, greater than 0.");
+
+     } else if ( /^[0-9.,]+$/.test(maxNumber)) {
+
+            alert('Take a Guess between 1 and' + " " + maxNumber);
+     break;
+    }
+    }
+
+let random = Math.floor(Math.random()* maxNumber) + 1
+console.log(random)
+
+function userguess() {
+
+    let guessbox = Number(document.getElementById("guessboxvalue").value);
+
+let message = document.getElementById("message")
+
+     if(guessbox < 1 || guessbox > maxNumber) {
+        message.innerHTML = "That is not a number in range, try again!"
+
+            } else if(guessbox == random) {
+    message.innerHTML = "You got it!";
+
+}     else if(guessbox > random) {
+        message.innerHTML = "No, try a lower number!";
+
+    } else if (guessbox < random) {
+        message.innerHTML = "No, try a higher number!"; 
+    }
+    } 
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    if ((charCode < 48 || charCode > 57))
+        alert("That is not a number.");
+
+    return true;
+}
+
+
+
+
+
+
